25ccbf4f8134fefa1211ee0418133b7746853c1e
gksehfapd <1ehfapd@gmail.com>
2022-08-26 11:10:55 +0900

    Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,21 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 8080;
 
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res, next) => {
-  return res.send("hello");
-};
-
-const login = (req, res, next) => {
-  return res.send("login");
-};
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`server listening on port http://localhost:${PORT}`);
