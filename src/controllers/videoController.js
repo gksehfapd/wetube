87694e3d2422064d7fec5de5e1291b1bb8562276
gksehfapd <1ehfapd@gmail.com>
2022-09-01 13:17:51 +0900

    Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,16 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+
+export const see = (req, res) => res.render("watch", { pageTitle: "Videos" });
+
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "edit Videos" });
+
+export const search = (req, res) =>
+  res.send("Search", { pageTitle: "Search Videos" });
+export const upload = (req, res) =>
+  res.send("Upload", { pageTitle: "Upload Videos" });
+
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("Delete Videos");
+};
