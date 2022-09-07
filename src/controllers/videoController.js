111f5486d7024699cb1d37e123398f6cd4bf251a
gksehfapd <1ehfapd@gmail.com>
2022-09-07 17:24:19 +0900

    Creating a Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,6 +4,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -28,8 +29,18 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
 
   return res.redirect("/");
 };
