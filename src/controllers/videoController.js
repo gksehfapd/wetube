c32b67ef6f7d8b001574344caf8c792688ade6af
gksehfapd <1ehfapd@gmail.com>
2022-09-15 16:43:26 +0900

    status code

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,7 +10,9 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Uh-Oh.. Video not found." });
+    return res
+      .status(404)
+      .render("404", { pageTitle: "Uh-Oh.. Video not found." });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
@@ -19,7 +21,9 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("edit", { pageTitle: "Uh-Oh.. Video not found." });
+    return res
+      .status(404)
+      .render("edit", { pageTitle: "Uh-Oh.. Video not found." });
   }
   return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
@@ -54,7 +58,7 @@ export const postUpload = async (req, res) => {
     return res.redirect("/");
   } catch (error) {
     console.log(error);
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
