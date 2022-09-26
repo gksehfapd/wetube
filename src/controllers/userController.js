7325e46f08552a366abe4b165b396aec973b9891
gksehfapd <1ehfapd@gmail.com>
2022-09-26 11:26:48 +0900

    Logged In User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,7 +57,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
-  console.log("USER LOG IN! COMING SOON!");
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
