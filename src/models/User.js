eb27e4690e2e7e445c8e2a81d80d71e77b9205f9
gksehfapd <1ehfapd@gmail.com>
2022-11-13 22:59:56 +0900

    feat:github login

--- src/models/User.js
+++ src/models/User.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  avatarUrl: String,
   email: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   location: String,
 });
 
