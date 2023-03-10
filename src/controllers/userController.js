64bf726664e2abb1c77ec3a0de97fe2a7475a489
gksehfapd <1ehfapd@gmail.com>
2023-03-10 20:36:07 +0900

    feat: profile avatar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -148,14 +148,16 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
 	const {
 		session: {
-			user: { _id }
+			user: { _id, avatarUrl }
 		},
-		body: { name, email, username, location }
+		body: { name, email, username, location },
+		file
 	} = req
 
 	const updateUser = await User.findByIdAndUpdate(
 		_id,
 		{
+			avatarUrl: file ? file.path : avatarUrl,
 			name,
 			email,
 			username,
