fae02ed763d6d7d4a4a277f4ce7dc25c523b7d80
gksehfapd <1ehfapd@gmail.com>
2023-03-12 01:01:34 +0900

    feat: thumbnail

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -18,6 +18,6 @@ videoRouter
 	.route('/upload')
 	.all(protectorMiddleware)
 	.get(getUpload)
-	.post(videoUpload.single('video'), postUpload)
+	.post(videoUpload.fields([{ name: 'video' }, { name: 'thumb' }]), postUpload)
 
 export default videoRouter
