ee46615b791e5e3e9ab3c01731eb123c2d2f5e53
gksehfapd <1ehfapd@gmail.com>
2023-03-11 18:53:07 +0900

    style: videoplayer style

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,12 +1,14 @@
 const video = document.querySelector('video')
 const playBtn = document.getElementById('play')
+const playBtnIcon = playBtn.querySelector('i')
 const muteBtn = document.getElementById('mute')
-const time = document.getElementById('time')
+const muteBtnIcon = muteBtn.querySelector('i')
 const volumeRange = document.getElementById('volume')
-const currentTime = document.getElementById('currentTime')
+const currenTime = document.getElementById('currenTime')
 const totalTime = document.getElementById('totalTime')
 const timeline = document.getElementById('timeline')
 const fullScreenBtn = document.getElementById('fullScreen')
+const fullScreenIcon = fullScreenBtn.querySelector('i')
 const videoContainer = document.getElementById('videoContainer')
 const videoControls = document.getElementById('videoControls')
 
@@ -14,14 +16,13 @@ let controlsTimeout = null
 let controlsMovementTimeout = null
 let volumeValue = 0.5
 video.volume = volumeValue
-
 const handlePlayClick = (e) => {
 	if (video.paused) {
 		video.play()
 	} else {
 		video.pause()
 	}
-	playBtn.innerText = video.paused ? 'Play' : 'Pause'
+	playBtnIcon.classList = video.paused ? 'fas fa-play' : 'fas fa-pause'
 }
 
 const handleMuteClick = (e) => {
@@ -30,7 +31,7 @@ const handleMuteClick = (e) => {
 	} else {
 		video.muted = true
 	}
-	muteBtn.innerText = video.muted ? 'Unmute' : 'Mute'
+	muteBtnIcon.classList = video.muted ? 'fas fa-volume-mute' : 'fas fa-volume-up'
 	volumeRange.value = video.muted ? 0 : volumeValue
 }
 
@@ -46,38 +47,34 @@ const handleVolumeChange = (event) => {
 	video.volume = value
 }
 
-const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substring(11, 19)
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(14, 5)
 
 const handleLoadedMetadata = () => {
 	totalTime.innerText = formatTime(Math.floor(video.duration))
 	timeline.max = Math.floor(video.duration)
 }
-
 const handleTimeUpdate = () => {
-	currentTime.innerText = formatTime(Math.floor(video.currentTime))
+	currenTime.innerText = formatTime(Math.floor(video.currentTime))
 	timeline.value = Math.floor(video.currentTime)
 }
-
 const handleTimelineChange = (event) => {
 	const {
 		target: { value }
 	} = event
 	video.currentTime = value
 }
-
 const handleFullscreen = () => {
 	const fullscreen = document.fullscreenElement
 	if (fullscreen) {
 		document.exitFullscreen()
-		fullScreenBtn.innerText = 'Enter Full Screen'
+		fullScreenIcon.classList = 'fas fa-expand'
 	} else {
 		videoContainer.requestFullscreen()
-		fullScreenBtn.innerText = 'Exit Full Screen'
+		fullScreenIcon.classList = 'fas fa-compress'
 	}
 }
 
 const hideControls = () => videoControls.classList.remove('showing')
-
 const handleMouseMove = () => {
 	if (controlsTimeout) {
 		clearTimeout(controlsTimeout)
@@ -88,19 +85,17 @@ const handleMouseMove = () => {
 		controlsMovementTimeout = null
 	}
 	videoControls.classList.add('showing')
-	controlsMovementTimeout = setTimeout(hideControls, 2000)
+	controlsMovementTimeout = setTimeout(hideControls, 3000)
 }
-
 const handleMouseLeave = () => {
-	controlsTimeout = setTimeout(hideControls, 1500)
+	controlsTimeout = setTimeout(hideControls, 3000)
 }
-
 playBtn.addEventListener('click', handlePlayClick)
 muteBtn.addEventListener('click', handleMuteClick)
 volumeRange.addEventListener('input', handleVolumeChange)
-video.addEventListener('loadedmetadata', handleLoadedMetadata)
+video.addEventListener('loadeddata', handleLoadedMetadata)
 video.addEventListener('timeupdate', handleTimeUpdate)
+videoContainer.addEventListener('mousemove', handleMouseMove)
+videoContainer.addEventListener('mouseleave', handleMouseLeave)
 timeline.addEventListener('input', handleTimelineChange)
 fullScreenBtn.addEventListener('click', handleFullscreen)
-video.addEventListener('mousemove', handleMouseMove)
-video.addEventListener('mouseleave', handleMouseLeave)
