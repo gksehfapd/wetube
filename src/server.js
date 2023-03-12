5463c988af42982196ce44a0aaf36c876882690a
gksehfapd <1ehfapd@gmail.com>
2023-03-12 10:21:16 +0900

    feat: flash message

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
+import flash from 'express-flash'
 import rootRouter from './routers/rootRouter'
 import videoRouter from './routers/videoRouter'
 import userRouter from './routers/userRouter'
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 	next()
 })
 
+app.use(flash())
 app.use(localsMiddleware)
 app.use('/uploads', express.static('uploads'))
 app.use('/static', express.static('assets'))
