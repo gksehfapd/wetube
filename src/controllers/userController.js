34392d17057c85735c3b449177171364266ebbfe
gksehfapd <1ehfapd@gmail.com>
2023-03-20 01:58:52 +0900

    isHeroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -155,10 +155,11 @@ export const postEdit = async (req, res) => {
 		file
 	} = req
 
+	const isHeroku = process.env.NODE_ENV === 'production'
 	const updateUser = await User.findByIdAndUpdate(
 		_id,
 		{
-			avatarUrl: file ? file.location : avatarUrl,
+			avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
 			name,
 			email,
 			username,
--- src/middlewares.js
+++ src/middlewares.js
@@ -10,16 +10,36 @@ const s3 = new S3Client({
 	}
 })
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === 'production'
+
+const s3ImageUploader = multerS3({
 	s3: s3,
 	bucket: 'dmtube',
-	acl: 'public-read'
+	acl: 'public-read',
+	key: function (request, file, ab_callback) {
+		const newFileName = Date.now() + '-' + file.originalname
+		const fullPath = 'images/' + newFileName
+		ab_callback(null, fullPath)
+	}
+})
+
+const s3VideoUploader = multerS3({
+	s3: s3,
+	bucket: 'dmtube',
+	acl: 'public-read',
+	contentType: multerS3.AUTO_CONTENT_TYPE,
+	key: function (request, file, ab_callback) {
+		const newFileName = Date.now() + '-' + file.originalname
+		const fullPath = 'videos/' + newFileName
+		ab_callback(null, fullPath)
+	}
 })
 
 export const localsMiddleware = (req, res, next) => {
 	res.locals.loggedIn = Boolean(req.session.loggedIn)
 	res.locals.siteName = 'Wetube'
 	res.locals.loggedInUser = req.session.user || {}
+	res.locals.isHeroku = isHeroku
 	next()
 }
 
@@ -46,7 +66,7 @@ export const avatarUpload = multer({
 	limits: {
 		fileSize: 3000000
 	},
-	storage: multerUploader
+	storage: isHeroku ? s3ImageUploader : undefined
 })
 
 export const videoUpload = multer({
@@ -54,5 +74,5 @@ export const videoUpload = multer({
 	limits: {
 		fileSize: 10240000
 	},
-	storage: multerUploader
+	storage: isHeroku ? s3VideoUploader : undefined
 })
